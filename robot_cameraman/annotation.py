87d62db8799e7c34e36b569d27bde6da967be3c6
Michael Maier <maier1michael@gmail.com>
2019-08-30 17:45:47 +0200

    move `ImageAnnotator` and draw functions to annotations.py

---
+++ robot_cameraman/annotation.py
@@ -0,0 +1,84 @@
+from typing import Optional, Dict, List, Tuple, NamedTuple
+
+import PIL.Image
+import PIL.ImageDraw
+from PIL.ImageDraw import ImageDraw
+from PIL.ImageFont import FreeTypeFont
+from edgetpu.detection.engine import DetectionCandidate
+
+from robot_cameraman.box import center, Box
+from robot_cameraman.tracking import Destination
+
+
+class Target(NamedTuple):
+    box: Box
+    detection_candidate: DetectionCandidate
+
+
+class ImageAnnotator:
+    target: Optional[Target] = None
+
+    def __init__(
+            self,
+            target_label_id: int,
+            labels: Dict[int, str],
+            font: FreeTypeFont) -> None:
+        self.target_label_id = target_label_id
+        self.labels = labels
+        self.font = font
+
+    def annotate(
+            self,
+            image: PIL.Image.Image,
+            inference_results: List[DetectionCandidate]) -> Optional[Target]:
+        draw = PIL.ImageDraw.Draw(image)
+        # Iterate through result list. Note that results are already sorted by
+        # confidence score (highest to lowest) and records with a lower score
+        # than the threshold are already removed.
+        target_box_found = False
+        for idx, obj in enumerate(inference_results):
+            box = obj.bounding_box.flatten().tolist()
+            if obj.label_id != self.target_label_id:
+                color = (255, 255, 255)
+            else:
+                color = (0, 255, 0)
+                if not target_box_found:
+                    target_box_found = True
+                    self.target = Target(box, obj)
+            self.draw_annotated_box(draw, box, obj, color)
+        if self.target is None or target_box_found:
+            return
+        self.draw_annotated_box(draw, self.target.box,
+                                self.target.detection_candidate,
+                                (255, 0, 0))
+
+    def draw_annotated_box(
+            self,
+            draw: ImageDraw,
+            box: List[float],
+            obj: DetectionCandidate,
+            color: Tuple[int, int, int]) -> None:
+        draw.rectangle(box, outline=color)
+        draw_point(draw, center(box), color)
+        # Annotate image with label and confidence score
+        display_str = self.labels[obj.label_id] + ": " + str(
+            round(obj.score * 100, 2)) + "%"
+        draw.text((box[0], box[1]), display_str, font=self.font)
+
+
+def draw_destination(
+        image: PIL.Image.Image,
+        destination: Destination,
+        color: Tuple[int, int, int] = (255, 0, 255)) -> None:
+    draw = PIL.ImageDraw.Draw(image)
+    draw_point(draw, destination.center, color)
+    draw.rectangle(destination.box, outline=color)
+
+
+def draw_point(
+        draw: ImageDraw,
+        point: Tuple[float, float],
+        color: Tuple[int, int, int],
+        radius: int = 3) -> None:
+    x, y = point
+    draw.ellipse((x - radius, y - radius, x + radius, y + radius), fill=color)
--- robot_cameraman/panasonic_cameraman.py
+++ robot_cameraman/panasonic_cameraman.py
@@ -15,7 +15,6 @@ import threading
 import time
 from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 from pathlib import Path
-from typing import Dict, List, Tuple, Optional, NamedTuple
 
 import PIL.Image
 import PIL.ImageDraw
@@ -23,13 +22,10 @@ import PIL.ImageFont
 import cv2
 import edgetpu.detection.engine
 import numpy
-from PIL.ImageDraw import ImageDraw
-from PIL.ImageFont import FreeTypeFont
-from edgetpu.detection.engine import DetectionCandidate
 from imutils.video import FPS
 
 from panasonic_camera.live_view import LiveView
-from robot_cameraman.box import center, Box
+from robot_cameraman.annotation import ImageAnnotator, draw_destination
 from robot_cameraman.resource import read_label_file
 from robot_cameraman.tracking import Destination, CameraController
 
@@ -39,80 +35,6 @@ to_exit: bool = False
 server_image: PIL.Image
 server: ThreadingHTTPServer
 live_view: LiveView
-
-
-class Target(NamedTuple):
-    box: Box
-    detection_candidate: DetectionCandidate
-
-
-class ImageAnnotator:
-    target: Optional[Target] = None
-
-    def __init__(
-            self,
-            target_label_id: int,
-            labels: Dict[int, str],
-            font: FreeTypeFont) -> None:
-        self.target_label_id = target_label_id
-        self.labels = labels
-        self.font = font
-
-    def annotate(
-            self,
-            image: PIL.Image.Image,
-            inference_results: List[DetectionCandidate]) -> Optional[Target]:
-        draw = PIL.ImageDraw.Draw(image)
-        # Iterate through result list. Note that results are already sorted by
-        # confidence score (highest to lowest) and records with a lower score
-        # than the threshold are already removed.
-        target_box_found = False
-        for idx, obj in enumerate(inference_results):
-            box = obj.bounding_box.flatten().tolist()
-            if obj.label_id != self.target_label_id:
-                color = (255, 255, 255)
-            else:
-                color = (0, 255, 0)
-                if not target_box_found:
-                    target_box_found = True
-                    self.target = Target(box, obj)
-            self.draw_annotated_box(draw, box, obj, color)
-        if self.target is None or target_box_found:
-            return
-        self.draw_annotated_box(draw, self.target.box,
-                                self.target.detection_candidate,
-                                (255, 0, 0))
-
-    def draw_annotated_box(
-            self,
-            draw: ImageDraw,
-            box: List[float],
-            obj: DetectionCandidate,
-            color: Tuple[int, int, int]) -> None:
-        draw.rectangle(box, outline=color)
-        draw_point(draw, center(box), color)
-        # Annotate image with label and confidence score
-        display_str = self.labels[obj.label_id] + ": " + str(
-            round(obj.score * 100, 2)) + "%"
-        draw.text((box[0], box[1]), display_str, font=self.font)
-
-
-def draw_destination(
-        image: PIL.Image.Image,
-        destination: Destination,
-        color: Tuple[int, int, int] = (255, 0, 255)) -> None:
-    draw = PIL.ImageDraw.Draw(image)
-    draw_point(draw, destination.center, color)
-    draw.rectangle(destination.box, outline=color)
-
-
-def draw_point(
-        draw: ImageDraw,
-        point: Tuple[float, float],
-        color: Tuple[int, int, int],
-        radius: int = 3) -> None:
-    x, y = point
-    draw.ellipse((x - radius, y - radius, x + radius, y + radius), fill=color)
 
 
 class RobotCameramanHttpHandler(BaseHTTPRequestHandler):
