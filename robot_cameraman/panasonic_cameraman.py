690ddf4adf9af6d252fded1e865c69c1b6bcf3f9
Michael Maier <maier1michael@gmail.com>
2019-08-30 17:36:59 +0200

    move classes `Destination` and `CameraController` to tracking.py

--- robot_cameraman/panasonic_cameraman.py
+++ robot_cameraman/panasonic_cameraman.py
@@ -31,7 +31,7 @@ from imutils.video import FPS
 from panasonic_camera.live_view import LiveView
 from robot_cameraman.box import center, Box
 from robot_cameraman.resource import read_label_file
-from simplebgc.serial_example import rotate_gimbal
+from robot_cameraman.tracking import Destination, CameraController
 
 # Variable to store command line arguments
 ARGS = None
@@ -97,17 +97,6 @@ class ImageAnnotator:
         draw.text((box[0], box[1]), display_str, font=self.font)
 
 
-class Destination:
-
-    def __init__(self, image_size: Tuple[int, int], variance: int = 50) -> None:
-        width, height = image_size
-        x, y = width / 2, height / 2
-        self.center = (x, y)
-        self.box = (x - variance, 0,
-                    x + variance, height)
-        self.variance = variance
-
-
 def draw_destination(
         image: PIL.Image.Image,
         destination: Destination,
@@ -124,63 +113,6 @@ def draw_point(
         radius: int = 3) -> None:
     x, y = point
     draw.ellipse((x - radius, y - radius, x + radius, y + radius), fill=color)
-
-
-class CameraController:
-
-    def __init__(
-            self,
-            destination: Destination,
-            max_allowed_speed: int = 1000) -> None:
-        self.destination = destination
-        self.max_allowed_speed = max_allowed_speed
-        self.search_speed = round(max_allowed_speed / 2)
-        self.yaw_speed = 0
-
-    def is_camera_moving(self) -> bool:
-        return self.yaw_speed != 0
-
-    def stop(self) -> None:
-        self.rotate(0)
-
-    def rotate(self, yaw_speed: int) -> None:
-        if self.yaw_speed != yaw_speed:
-            self.yaw_speed = yaw_speed
-            rotate_gimbal(self.yaw_speed)
-
-    def update(self, target_box: Box) -> None:
-        if target_box is None:
-            self.search_target()
-        else:
-            self.move_to_target(target_box)
-
-    def move_to_target(self, target_box: Box) -> None:
-        tx, ty = center(target_box)
-        dx, dy = self.destination.center
-        distance = tx - dx
-        # print(distance)
-        abs_distance = abs(distance)
-        if abs_distance < self.destination.variance:
-            if self.is_camera_moving():
-                self.stop()
-        else:
-            # TODO check speed range of 2s is -32,768 to 32,767
-            # image_width / speed_steps = 640 / 20 = 32
-            # max_allowed_speed / speed_steps = 1000 / 20 = 100
-            speed = round(abs_distance / 32 * 100)
-            speed = min(self.max_allowed_speed, speed)
-            if distance < 0:
-                speed = -speed
-            self.rotate(speed)
-
-    def rotate_right(self) -> None:
-        self.rotate(100)
-
-    def rotate_left(self) -> None:
-        self.rotate(-100)
-
-    def search_target(self) -> None:
-        self.rotate(self.search_speed)
 
 
 class RobotCameramanHttpHandler(BaseHTTPRequestHandler):
---
+++ robot_cameraman/tracking.py
@@ -0,0 +1,72 @@
+from typing import Tuple
+
+from robot_cameraman.box import Box, center
+from simplebgc.serial_example import rotate_gimbal
+
+
+class Destination:
+
+    def __init__(self, image_size: Tuple[int, int], variance: int = 50) -> None:
+        width, height = image_size
+        x, y = width / 2, height / 2
+        self.center = (x, y)
+        self.box = (x - variance, 0,
+                    x + variance, height)
+        self.variance = variance
+
+
+class CameraController:
+
+    def __init__(
+            self,
+            destination: Destination,
+            max_allowed_speed: int = 1000) -> None:
+        self.destination = destination
+        self.max_allowed_speed = max_allowed_speed
+        self.search_speed = round(max_allowed_speed / 2)
+        self.yaw_speed = 0
+
+    def is_camera_moving(self) -> bool:
+        return self.yaw_speed != 0
+
+    def stop(self) -> None:
+        self.rotate(0)
+
+    def rotate(self, yaw_speed: int) -> None:
+        if self.yaw_speed != yaw_speed:
+            self.yaw_speed = yaw_speed
+            rotate_gimbal(self.yaw_speed)
+
+    def update(self, target_box: Box) -> None:
+        if target_box is None:
+            self.search_target()
+        else:
+            self.move_to_target(target_box)
+
+    def move_to_target(self, target_box: Box) -> None:
+        tx, ty = center(target_box)
+        dx, dy = self.destination.center
+        distance = tx - dx
+        # print(distance)
+        abs_distance = abs(distance)
+        if abs_distance < self.destination.variance:
+            if self.is_camera_moving():
+                self.stop()
+        else:
+            # TODO check speed range of 2s is -32,768 to 32,767
+            # image_width / speed_steps = 640 / 20 = 32
+            # max_allowed_speed / speed_steps = 1000 / 20 = 100
+            speed = round(abs_distance / 32 * 100)
+            speed = min(self.max_allowed_speed, speed)
+            if distance < 0:
+                speed = -speed
+            self.rotate(speed)
+
+    def rotate_right(self) -> None:
+        self.rotate(100)
+
+    def rotate_left(self) -> None:
+        self.rotate(-100)
+
+    def search_target(self) -> None:
+        self.rotate(self.search_speed)
