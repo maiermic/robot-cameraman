f4003b7dbe4bce8a8e6956bb0c47590c38069598
Michael Maier <maier1michael@gmail.com>
2019-08-31 23:19:52 +0200

    add debug log before and after image is received

--- panasonic_camera/live_view.py
+++ panasonic_camera/live_view.py
@@ -1,4 +1,8 @@
 import socket
+import logging
+from logging import Logger
+
+logger: Logger = logging.getLogger(__name__)
 
 
 class LiveView:
@@ -17,7 +21,9 @@ class LiveView:
         """
         bufsize = 30000
         offset = 132
+        logger.debug('receive image')
         data, addr = self.sock.recvfrom(bufsize)
+        logger.debug('image received')
         for i in range(130, 320):
             if data[i] == 0xFF and data[i + 1] == 0xD8:
                 offset = i
