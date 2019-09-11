37b91660eb416d3ef05040c8108ee33808711c78
Michael Maier <maier1michael@gmail.com>
2019-09-11 09:12:06 +0200

    add box that is created from center and size

--- robot_cameraman/box.py
+++ robot_cameraman/box.py
@@ -40,6 +40,10 @@ class Box(Protocol):
         x1, y1, x2, y2 = coordinates
         return TwoPointsBox(x1, y1, x2, y2)
 
+    @staticmethod
+    def from_center_and_size(center: Point, width: float, height: float):
+        return CenterSizeBox(center, width, height)
+
 
 class TwoPointsBox(Box):
 
@@ -49,4 +53,21 @@ class TwoPointsBox(Box):
         self.width = abs(x2 - x1)
         self.height = abs(y2 - y1)
         self.center = Point(abs(x1 + x2) / 2, abs(y1 + y2) / 2)
+        self.coordinates = [x1, y1, x2, y2]
+
+
+class CenterSizeBox(Box):
+
+    def __init__(self, center: Point, width: float, height: float) -> None:
+        half_width = width / 2
+        half_height = height / 2
+        x1 = center.x - half_width
+        x2 = center.x + half_width
+        y1 = center.y - half_height
+        y2 = center.y + half_height
+        self.x = x1
+        self.y = y1
+        self.width = width
+        self.height = height
+        self.center = center
         self.coordinates = [x1, y1, x2, y2]
