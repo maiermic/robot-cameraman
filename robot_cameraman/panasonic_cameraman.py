6e359e999578f71c281d2b12aa375daef6c752a9
Michael Maier <maier1michael@gmail.com>
2019-10-05 18:50:04 +0200

    rename window title to 'Robot Cameraman'

--- robot_cameraman/panasonic_cameraman.py
+++ robot_cameraman/panasonic_cameraman.py
@@ -103,7 +103,7 @@ class PanasonicCameraman:
                 if self._output:
                     self._output.write(cv2_image)
                 if 'DISPLAY' in os.environ:
-                    cv2.imshow('NCS Improved live inference', cv2_image)
+                    cv2.imshow('Robot Cameraman', cv2_image)
 
                 # Display the frame for 5ms, and close the window so that the
                 # next frame can be displayed. Close the window if 'q' or 'Q'
