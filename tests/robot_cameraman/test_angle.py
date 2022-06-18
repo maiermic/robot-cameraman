1cabd837caf25ca8d3f7a07cfa12e51a6d3745d2
Michael Maier <maier1michael@gmail.com>
2022-06-18 16:50:36 +0200

    test: swap operands in comparison of assertion, since right side is expected value

--- tests/robot_cameraman/test_angle.py
+++ tests/robot_cameraman/test_angle.py
@@ -3,14 +3,14 @@ from robot_cameraman.angle import get_delta_angle_clockwise, \
 
 
 def test_get_delta_angle_clockwise():
-    assert 6 == get_delta_angle_clockwise(left=0, right=6)
-    assert 354 == get_delta_angle_clockwise(left=6, right=0)
-    assert 0 == get_delta_angle_clockwise(left=0, right=0)
-    assert 5 == get_delta_angle_clockwise(left=30, right=35)
+    assert get_delta_angle_clockwise(left=0, right=6) == 6
+    assert get_delta_angle_clockwise(left=6, right=0) == 354
+    assert get_delta_angle_clockwise(left=0, right=0) == 0
+    assert get_delta_angle_clockwise(left=30, right=35) == 5
 
 
 def test_get_delta_angle_counter_clockwise():
-    assert 354 == get_delta_angle_counter_clockwise(left=0, right=6)
-    assert 6 == get_delta_angle_counter_clockwise(left=6, right=0)
-    assert 0 == get_delta_angle_counter_clockwise(left=0, right=0)
-    assert 5 == get_delta_angle_counter_clockwise(left=35, right=30)
+    assert get_delta_angle_counter_clockwise(left=0, right=6) == 354
+    assert get_delta_angle_counter_clockwise(left=6, right=0) == 6
+    assert get_delta_angle_counter_clockwise(left=0, right=0) == 0
+    assert get_delta_angle_counter_clockwise(left=35, right=30) == 5
