fb8672b57bc50911003ba75bfe435ac43203c351
Michael Maier <maier1michael@gmail.com>
2022-07-06 19:41:05 +0200

    add TiltInvertedGimbal

--- robot_cameraman/gimbal.py
+++ robot_cameraman/gimbal.py
@@ -54,6 +54,46 @@ class SimpleBgcGimbal(simplebgc.gimbal.Gimbal, Gimbal):
     pass
 
 
+class TiltInvertedGimbal(Gimbal):
+    """Wrap another gimbal to invert its tilt direction.
+    If the camera is mounted on the gimbal in the opposite direction (rotated
+    180 degree), the tilt speed and angle has to be inverted.
+
+    See https://github.com/maiermic/robot-cameraman/issues/46
+    """
+
+    def __init__(self, gimbal: Gimbal) -> None:
+        self._gimbal = gimbal
+
+    def control(self, yaw_mode: ControlMode = ControlMode.speed,
+                yaw_speed: float = 0, yaw_angle: float = 0,
+                pitch_mode: ControlMode = ControlMode.speed,
+                pitch_speed: float = 0, pitch_angle: float = 0,
+                roll_mode: ControlMode = ControlMode.speed,
+                roll_speed: float = 0, roll_angle: float = 0) -> None:
+        self._gimbal.control(yaw_mode, yaw_speed, yaw_angle,
+                             # invert speed and angle
+                             pitch_mode, -pitch_speed, -pitch_angle,
+                             roll_mode, roll_speed, roll_angle)
+
+    def stop(self) -> None:
+        self._gimbal.stop()
+
+    def get_angles(self) -> GetAnglesInCmd:
+        angles = self._gimbal.get_angles()
+        return GetAnglesInCmd(
+            imu_angle_1=angles.imu_angle_1,
+            target_angle_1=angles.target_angle_1,
+            target_speed_1=angles.target_speed_1,
+            imu_angle_2=angles.imu_angle_2,
+            # invert tilt speed and angle
+            target_angle_2=-angles.target_angle_2,
+            target_speed_2=-angles.target_speed_2,
+            imu_angle_3=angles.imu_angle_3,
+            target_angle_3=angles.target_angle_3,
+            target_speed_3=angles.target_speed_3)
+
+
 class DummyGimbal(Gimbal):
     def control(self, yaw_mode: ControlMode = ControlMode.speed,
                 yaw_speed: float = 0, yaw_angle: float = 0,
