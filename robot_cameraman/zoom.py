f03e01579dc14a1b6242ac18e462f6bf66b75d23
Michael Maier <maier1michael@gmail.com>
2022-09-18 12:35:42 +0200

    feat: add class `ZoomSteps` to manage and function to parse `ZoomStep`s

--- robot_cameraman/zoom.py
+++ robot_cameraman/zoom.py
@@ -1,5 +1,9 @@
+import json
 from dataclasses import dataclass
-from typing import Optional
+from pathlib import Path
+from typing import Optional, List
+
+import more_itertools
 
 
 @dataclass()
@@ -28,3 +32,39 @@ class ZoomStep:
         self.min_stop_zoom_in_time = min_stop_zoom_in_time
         self.max_stop_zoom_in_time = max_stop_zoom_in_time
         self.zoom_out_time = zoom_out_time
+
+
+@dataclass()
+class ZoomSteps:
+    _zoom_steps: List[ZoomStep]
+
+    def __init__(self, zoom_steps: List[ZoomStep]) -> None:
+        self._zoom_steps = zoom_steps
+
+    def get_by_zoom_ratio(self, zoom_ratio: float):
+        def has_zoom_ratio(zoom_step: ZoomStep):
+            return zoom_step.zoom_ratio == zoom_ratio
+
+        return more_itertools.first_true(
+            self._zoom_steps, None, has_zoom_ratio)
+
+    def get_next_greater(self, zoom_step: ZoomStep):
+        try:
+            i = self._zoom_steps.index(zoom_step)
+            return self._zoom_steps[i + 1]
+        except (ValueError, IndexError):
+            return None
+
+
+def parse_zoom_steps(file: Path) -> ZoomSteps:
+    with open(file) as file_descriptor:
+        config = json.load(file_descriptor)
+    return ZoomSteps(zoom_steps=[
+        ZoomStep(zoom_ratio=data['zoom_ratio'],
+                 zoom_in_time=data['zoom_in_time'],
+                 total_zoom_in_time=data['total_zoom_in_time'],
+                 stop_zoom_in_time=data['stop_zoom_in_time'],
+                 min_stop_zoom_in_time=data['min_stop_zoom_in_time'],
+                 max_stop_zoom_in_time=data['max_stop_zoom_in_time'],
+                 zoom_out_time=data['zoom_out_time'])
+        for data in config['slow']])
