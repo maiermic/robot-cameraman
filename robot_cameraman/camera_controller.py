bb751c0046adec0e94ff767388b0a4c7379054fa
Michael Maier <maier1michael@gmail.com>
2022-11-21 23:02:09 +0100

    refactor: rename _is_min_reached to is_min_reached

--- robot_cameraman/camera_controller.py
+++ robot_cameraman/camera_controller.py
@@ -319,7 +319,7 @@ class CameraZoomIndexLimitController(CameraZoomLimitController):
         logger.debug(
             f'check if current zoom index {self.zoom_index} reached limit')
         if (self._is_heading_towards_min(camera_speeds.zoom_speed)
-                and self._is_min_reached()):
+                and self.is_min_reached()):
             logger.debug('min zoom index reached, zoom speed is set to 0')
             camera_speeds.zoom_speed = ZoomSpeed.ZOOM_STOPPED
         if (self._is_heading_towards_max(camera_speeds.zoom_speed)
@@ -330,7 +330,7 @@ class CameraZoomIndexLimitController(CameraZoomLimitController):
     def _is_heading_towards_min(self, zoom_speed: ZoomSpeed):
         return self.min_zoom_index is not None and zoom_speed < 0
 
-    def _is_min_reached(self):
+    def is_min_reached(self):
         return self.zoom_index <= self.min_zoom_index
 
     def _is_heading_towards_max(self, zoom_speed: ZoomSpeed):
